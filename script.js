4d1a3baf672f0954dfccf340a9508a13d7687a60
grabis <ik4988629@gmail.com>
2025-07-05 17:52:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const mobileVideoSrc = 'video/1.mp4';   
     const desktopVideoFiles = [];
-    for (let i = 2; i <= 13; i++) { 
+    for (let i = 2; i <= 22; i++) { 
         desktopVideoFiles.push(`video/${i}.mp4`);
     }
 
